14977df28edc3c7878c82c3eeb284165685d7883
HenzoWTF <Urenahenzo@gmail.com>
2024-03-04 18:46:18 -0400

    Library terminada

---
+++ Library/Accesorios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Accesorios
+    {
+        [Key]
+        public int AccesoriosId { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Library/Vehiculo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Vehiculo
+    {
+        [Key]
+
+        [Required(ErrorMessage = "Debes de insertar un Id")]
+        public int VehiculoId { get; set; }
+
+
+        [Required(ErrorMessage = "Debe de insertar una Fecha")]
+        public DateTime Fecha { get; set;}
+
+
+        [Required(ErrorMessage = "Debes de insertar una descripción")]
+        public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Debes de insertar el costo del vehiculo")]
+        public decimal? Costo { get; set; }
+
+
+        [Required(ErrorMessage = "Debes de insertar los gastos")]
+        public decimal? Gastos { get; set; }
+
+        [ForeignKey("DetalleId")]
+        public ICollection<VehiculosDetalle> vehiculosDetalles { get; set; } = new HashSet<VehiculosDetalle>();
+    }
+}
