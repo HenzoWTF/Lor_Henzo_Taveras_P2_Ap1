3468950586d81d9906c8f9e8f4e625c77930db1d
HenzoWTF <Urenahenzo@gmail.com>
2024-03-04 19:10:41 -0400

    Base de datos creada con exito

---
+++ Lor_Henzo_Taveras_P2_Ap1.Api/Migrations/20240304230608_Inicial.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lor_Henzo_Taveras_P2_Ap1.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accesorios",
+                columns: table => new
+                {
+                    AccesoriosId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accesorios", x => x.AccesoriosId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehiculo",
+                columns: table => new
+                {
+                    VehiculoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    Costo = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Gastos = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehiculo", x => x.VehiculoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VehiculoDetalle",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "INTEGER", nullable: false),
+                    VehiculosId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AccesorrioId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Valor = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehiculoDetalle", x => x.DetalleId);
+                    table.ForeignKey(
+                        name: "FK_VehiculoDetalle_Vehiculo_DetalleId",
+                        column: x => x.DetalleId,
+                        principalTable: "Vehiculo",
+                        principalColumn: "VehiculoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accesorios");
+
+            migrationBuilder.DropTable(
+                name: "VehiculoDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Vehiculo");
+        }
+    }
+}
